7db55b519c110a0f0f395c3b902ceefb77d68c0a
wjyqt <2303082168@qq.com>
2024-03-13 23:58:24 +0800

    Site updated: 2024-03-13 23:58:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/03/部署hexo博客到gitee上踩过的坑/","2024/03/03/值得珍藏的照片/","2024/03/07/写给钦婷/"];function toRandomPost(){
+var posts=["2024/03/03/部署hexo博客到gitee上踩过的坑/","2024/03/03/值得珍藏的照片/","2024/03/07/写给钦婷/","2024/03/13/stm32笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
